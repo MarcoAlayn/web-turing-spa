7506fbbdbf2a7f652f00a907c3636b2e16d3eef4
MarcoAlayn <marc_rob92@hotmail.com>
2022-11-03 03:37:45 -0700

    second commit

---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="heroContainer">
+      <h1>TURING-IA</h1>
+    </div>
+  );
+}
+
+export default Hero;
